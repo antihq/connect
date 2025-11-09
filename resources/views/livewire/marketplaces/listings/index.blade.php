1a3b861e14d0dbbf642f49ffec44101b244c4cb7
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-09 17:49:39 -0600

    ui: show all public marketplace listings on listings index page

--- resources/views/livewire/marketplaces/listings/index.blade.php
+++ resources/views/livewire/marketplaces/listings/index.blade.php
@@ -1,11 +1,43 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\Marketplace;
+use App\Models\Listing;
 
 new class extends Component {
-    //
+    public Marketplace $marketplace;
+    public $listings;
+
+    public function mount()
+    {
+        $this->listings = Listing::where('marketplace_id', $this->marketplace->id)
+            ->where('status', 'public')
+            ->latest()
+            ->get();
+    }
 }; ?>
 
 <div>
-    //
+    @include('partials.marketplace-navbar', ['marketplace' => $marketplace])
+
+    <flux:table>
+        <flux:table.columns>
+            <flux:table.column>Title</flux:table.column>
+            <flux:table.column>Description</flux:table.column>
+            <flux:table.column>Created</flux:table.column>
+        </flux:table.columns>
+        <flux:table.rows>
+            @foreach ($listings as $listing)
+                <flux:table.row :key="$listing->id">
+                    <flux:table.cell>
+                        <flux:link :href="route('marketplaces.listings.show', [$marketplace, $listing])">
+                            {{ $listing->title }}
+                        </flux:link>
+                    </flux:table.cell>
+                    <flux:table.cell>{{ $listing->description }}</flux:table.cell>
+                    <flux:table.cell>{{ $listing->created_at->diffForHumans() }}</flux:table.cell>
+                </flux:table.row>
+            @endforeach
+        </flux:table.rows>
+    </flux:table>
 </div>
