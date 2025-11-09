1bfc836d533482204d9d55b6b26b6e85ff76bab9
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-09 17:47:50 -0600

    ui: add Listings nav item to marketplace navbar partial

--- resources/views/partials/marketplace-navbar.blade.php
+++ resources/views/partials/marketplace-navbar.blade.php
@@ -1,5 +1,6 @@
 <flux:navbar>
     <flux:navbar.item :href="route('marketplaces.show', $marketplace)">Home</flux:navbar.item>
+    <flux:navbar.item :href="route('marketplaces.listings.index', $marketplace)">Listings</flux:navbar.item>
     <flux:navbar.item :href="route('marketplaces.listings.create', $marketplace)">Post a new listings</flux:navbar.item>
     <flux:navbar.item :href="route('marketplaces.inbox.orders', $marketplace)">Inbox</flux:navbar.item>
     <flux:navbar.item :href="route('marketplaces.account.listings', $marketplace)">Profile</flux:navbar.item>
