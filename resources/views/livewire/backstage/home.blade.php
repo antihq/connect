37513c6f780efbe4b7b2798768ac36f07ecf2ed7
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-10 12:12:26 -0600

    ui: show marketplace name for current organization on backstage home (Livewire 3 computed property)

--- resources/views/livewire/backstage/home.blade.php
+++ resources/views/livewire/backstage/home.blade.php
@@ -1,11 +1,17 @@
 <?php
 
 use Livewire\Volt\Component;
+use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\Auth;
 
 new class extends Component {
-    //
+    #[Computed]
+    public function marketplace(): \App\Models\Marketplace
+    {
+        return Auth::user()->currentOrganization->marketplace;
+    }
 }; ?>
 
 <div>
-    //
+    Marketplace: {{ $this->marketplace->id }}
 </div>
