9c7ddd3ef332b0697b10bf4b7d45d896fa21dd59
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-11 20:36:12 -0600

    ui: add changelog entry component and update changelog view (Blade components, markup)

--- resources/views/livewire/changelog.blade.php
+++ resources/views/livewire/changelog.blade.php
@@ -13,5 +13,26 @@ new #[Layout('components.layouts.changelog'), Title('Changelog')] class extends 
 }; ?>
 
 <div>
-    lkjsdf
+    <x-changelog.entry date="2025-11-11">
+        <x-changelog.img src="/" />
+        <h2><a href="#changelog-2023-04-06">Culpa voluptate ea laborum nisi in est nulla.</a></h2>
+        <p>
+            Deserunt pariatur veniam velit elit voluptate.
+        </p>
+        <p>
+            Ullamco magna magna incididunt voluptate quis eu sit proident.
+        </p>
+        <h3>
+            <flux:icon.sparkles variant="solid" />
+            Improvements
+        </h3>
+        <ul>
+            <li>Sunt eu id cupidatat dolor ad dolore elit deserunt occaecat Lorem eiusmod aute.</li>
+            <li>Dolor aliqua ut id ullamco duis duis cupidatat mollit commodo pariatur.</li>
+            <li>Sunt eu id cupidatat dolor ad dolore elit deserunt occaecat Lorem eiusmod aute.</li>
+            <li>Dolor aliqua ut id ullamco duis duis cupidatat mollit commodo pariatur.</li>
+            <li>Sunt eu id cupidatat dolor ad dolore elit deserunt occaecat Lorem eiusmod aute.</li>
+            <li>Dolor aliqua ut id ullamco duis duis cupidatat mollit commodo pariatur.</li>
+        </ul>
+    </x-changelog.entry>
 </div>
