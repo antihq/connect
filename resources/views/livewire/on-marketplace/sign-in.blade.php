426561f5b152c6a8d9de0c55c70d72685062bde9
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-12 21:13:17 -0600

    core: auto-add user as marketplace organization member on login

--- resources/views/livewire/on-marketplace/sign-in.blade.php
+++ resources/views/livewire/on-marketplace/sign-in.blade.php
@@ -59,6 +59,12 @@ new class extends Component {
             ]
         );
         Auth::login($user, true);
+
+        $organization = $this->marketplace->organization;
+        if ($organization && !$organization->isMember($user)) {
+            $organization->addMember($user);
+        }
+
         $record->delete();
         return redirect('/dashboard');
     }
